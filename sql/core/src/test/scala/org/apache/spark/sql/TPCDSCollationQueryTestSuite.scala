f353cff966ac4ad10707ec1194bf1acca16e27d2
Nikola Mandic <nikola.mandic@databricks.com>
2024-03-26 17:59:57 +0100

    More work

--- sql/core/src/test/scala/org/apache/spark/sql/TPCDSCollationQueryTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/TPCDSCollationQueryTestSuite.scala
@@ -18,6 +18,7 @@
 package org.apache.spark.sql
 
 import java.io.File
+import java.nio.file.{Files, Paths}
 import java.util.Locale
 
 import org.apache.spark.{SparkConf, SparkContext}
@@ -25,6 +26,7 @@ import org.apache.spark.sql.catalyst.util.{fileToString, resourceToString, strin
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.TestSparkSession
 import org.apache.spark.tags.ExtendedSQLTest
+import org.apache.spark.util.Utils
 
 @ExtendedSQLTest
 class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQueryTestHelper {
@@ -39,98 +41,100 @@ class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQuer
     new TestSparkSession(new SparkContext("local[1]", this.getClass.getSimpleName, sparkConf))
   }
 
-  protected val baseResourcePath = {
+  if (tpcdsDataPath.nonEmpty) {
+    val nonExistentTables = tableColumns.keys.filterNot { tableName =>
+      Files.exists(Paths.get(s"${tpcdsDataPath.get}/$tableName"))
+    }
+    if (nonExistentTables.nonEmpty) {
+      fail(s"Non-existent TPCDS table paths found in ${tpcdsDataPath.get}: " +
+        nonExistentTables.mkString(", "))
+    }
+  }
+
+  protected val baseResourcePath: String = {
     getWorkspaceFilePath(
       "sql",
       "core",
       "src",
       "test",
       "resources",
-      "tpcds-query-collated-results").toFile.getAbsolutePath
+      "tpcds-query-collated-results"
+    ).toFile.getAbsolutePath
   }
 
-  val defaultDb = "tpcds_dev"
   val collatedDb = "tpcds_collated_dev"
   val normalizedDb = "tpcds_normalized_dev"
   val randomizeCase = "RANDOMIZE_CASE"
 
+  private def withDB[T](dbName: String)(fun: => T): T = {
+    Utils.tryWithSafeFinally({
+      spark.sql(s"USE `$dbName`")
+      fun
+    }) {
+      spark.sql("USE DEFAULT")
+    }
+  }
+
   override def createTables(): Unit = {
-    spark.sql(s"DROP DATABASE IF EXISTS $defaultDb CASCADE")
-    spark.sql(s"DROP DATABASE IF EXISTS $collatedDb CASCADE")
-    spark.sql(s"DROP DATABASE IF EXISTS $normalizedDb CASCADE")
-
-    spark.sql(s"CREATE DATABASE $defaultDb")
-    spark.sql(s"CREATE DATABASE $collatedDb")
-    spark.sql(s"CREATE DATABASE $normalizedDb")
-
     spark.udf.register(
       randomizeCase,
       functions.udf((s: String) => {
         val random = new scala.util.Random()
-        s.map(c => if (random.nextBoolean()) c.toUpper else c.toLower)
-      }))
-
-    super.createTables()
-  }
-
-  override def createTable(
-      spark: SparkSession,
-      tableName: String,
-      format: String = "parquet",
-      options: Seq[String]): Unit = {
-
-    val columns = tableColumns(tableName)
-      .split("\n")
-      .filter(_.trim.nonEmpty)
-      .map { column =>
-        if (column.trim.split("\\s+").length != 2) {
-          throw new IllegalArgumentException(s"Invalid column definition: $column")
+        s match {
+          case null => null
+          case _ => s.map(c => if (random.nextBoolean()) c.toUpper else c.toLower)
         }
-        val Array(name, colType) = column.trim.split("\\s+")
-        (name, colType.replaceAll(",$", ""))
-      }
+      }).asNondeterministic().asNonNullable()
+    )
+    Seq(
+      (collatedDb, "UTF8_BINARY_LCASE", randomizeCase),
+      (normalizedDb, "UTF8_BINARY", "LOWER")
+    ).foreach {
+      case (db, collation, transformationFn) =>
+        spark.sql(s"CREATE DATABASE `$db`")
+        withDB(db) {
+          tableNames.foreach(tableName => {
+            val columns = tableColumns(tableName)
+              .split("\n")
+              .filter(_.trim.nonEmpty)
+              .map { column =>
+                if (column.trim.split("\\s+").length != 2) {
+                  throw new IllegalArgumentException(s"Invalid column definition: $column")
+                }
+                val Array(name, colType) = column.trim.split("\\s+")
+                (name, colType.replaceAll(",$", ""))
+              }
 
-    def initTable(db: String, collation: String, transformationFn: String): Unit = {
-      spark.sql(
-        s"""
-           |CREATE TABLE $db.$tableName
-           |(${collateStringColumns(columns, collation)})
-           |USING $format
-           |${options.mkString("\n")}
-         """.stripMargin)
+            spark.sql(
+              s"""
+                 |CREATE TABLE `$tableName` (${collateStringColumns(columns, collation)})
+                 |USING parquet
+                 |""".stripMargin)
 
-      val transformedColumns = columns.map { case (name, colType) =>
-        if (isTextColumn(colType)) {
-          s"$transformationFn($name) AS $name"
-        } else {
-          name
+            val transformedColumns = columns.map { case (name, colType) =>
+              if (isTextColumn(colType)) {
+                s"$transformationFn($name) AS $name"
+              } else {
+                name
+              }
+            }.mkString(", ")
+
+            spark.sql(
+              s"""
+                 |INSERT INTO TABLE `$tableName`
+                 |SELECT $transformedColumns
+                 |FROM parquet.`${tpcdsDataPath.get}/$tableName`
+                 |""".stripMargin)
+          })
         }
-      }.mkString(", ")
-
-      spark.sql(
-        s"""
-          |INSERT INTO TABLE $db.$tableName
-          |SELECT $transformedColumns
-          |FROM $defaultDb.$tableName
-          |""".stripMargin)
     }
-
-    spark.sql(
-      s"""
-         |CREATE TABLE $defaultDb.$tableName (${tableColumns(tableName)})
-         |USING $format
-         |${options.mkString("\n")}
-   """.stripMargin)
-
-    initTable(collatedDb, "UTF8_BINARY_LCASE", randomizeCase)
-    initTable(normalizedDb, "UTF8_BINARY", "UPPER")
-    val x = 3
   }
 
-  override def dropTables(): Unit = {
-    spark.sql(s"DROP DATABASE IF EXISTS $collatedDb CASCADE")
-    spark.sql(s"DROP DATABASE IF EXISTS $normalizedDb CASCADE")
-  }
+  override def dropTables(): Unit =
+    Seq(collatedDb, normalizedDb).foreach(db => {
+      withDB(db)(super.dropTables())
+      spark.sql(s"DROP DATABASE `$db`")
+    })
 
   private def collateStringColumns(
       columns: Array[(String, String)],
@@ -153,23 +157,19 @@ class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQuer
   private def runQuery(query: String, goldenFile: File, conf: Map[String, String]): Unit = {
     withSQLConf(conf.toSeq: _*) {
       try {
-        val collatedOutput = getQueryOutput(collatedDb, query)
-        val normalizedOutput = getQueryOutput(normalizedDb, query)
+        val collatedOutput = withDB(collatedDb)(getQueryOutput(query).toLowerCase(Locale.ROOT))
+        val normalizedOutput = withDB(normalizedDb)(getQueryOutput(query))
         val queryString = query.trim
-
-        if (regenerateGoldenFiles || true) {
-//          writeNewGoldenFile(collatedOutput, normalizedOutput, goldenFile)
+        if (regenerateGoldenFiles) {
+          writeNewGoldenFile(collatedOutput, normalizedOutput, goldenFile)
         }
-
         val (expectedCollatedOutput, expectedNormalizedOutput) = readGoldenFile(goldenFile)
-
         assertResult(expectedCollatedOutput, s"Collated output did not match\n$queryString") {
           collatedOutput
         }
         assertResult(expectedNormalizedOutput, s"Normalized output did not match\n$queryString") {
           normalizedOutput
         }
-
       } catch {
         case e: Throwable =>
           val configs = conf.map { case (k, v) =>
@@ -180,9 +180,7 @@ class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQuer
     }
   }
 
-  private def getQueryOutput(db: String, query: String): String = {
-    spark.sql(s"USE $db")
-
+  private def getQueryOutput(query: String): String = {
     val (_, output) = handleExceptions(getNormalizedQueryExecutionResult(spark, query))
     output.mkString("\n").replaceAll("\\s+$", "")
   }
@@ -193,9 +191,9 @@ class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQuer
       goldenFile: File): Unit = {
     val goldenOutput = {
       s"-- Automatically generated by ${getClass.getSimpleName}\n\n" +
-        s"-- !query on collated db\n" +
+        s"-- !query output on collated data\n" +
         collatedOutput + "\n" +
-        s"-- !query on normalized db\n" +
+        s"-- !query output on normalized data\n" +
         normalizedOutput +
         "\n"
     }
@@ -219,7 +217,7 @@ class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQuer
     (segments(1).trim, segments(2).replaceAll("\\s+$", ""))
   }
 
-//  if (tpcdsDataPath.nonEmpty) {
+  if (tpcdsDataPath.nonEmpty) {
     tpcdsQueries.foreach { name =>
       val queryString = resourceToString(
         s"tpcds/$name.sql",
@@ -239,7 +237,7 @@ class TPCDSCollationQueryTestSuite extends QueryTest with TPCDSBase with SQLQuer
         runQuery(queryString, goldenFile, Map.empty)
       }
     }
-//  } else {
-//    ignore("skipped because env 'SPARK_TPCDS_DATA' is not set") {}
-//  }
+  } else {
+    ignore("skipped because env 'SPARK_TPCDS_DATA' is not set") {}
+  }
 }
